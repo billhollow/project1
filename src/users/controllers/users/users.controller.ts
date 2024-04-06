aa67c7fbaefa44fb04a5831aa542c579cf808330
billhollow <40575441+billhollow@users.noreply.github.com>
2024-04-06 08:57:25 -0500

    add User, Posts and Profiles applications
    Add Entities, controllers, dtos and services

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,52 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { CreateUserPostDto } from 'src/users/dtos/CreateUserPost.dto';
+import { CreateUserProfileDto } from 'src/users/dtos/CreateUserProfile.dto';
+import { UpdateUserDto } from 'src/users/dtos/UpdateUser.dto';
+import { UsersService } from 'src/users/services/users/users.service';
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private userService: UsersService) {}
+
+    @Get()
+    async getUsers(){
+        return this.userService.getUsers();
+    }
+
+    @Post()
+    createUser(@Body() createUserDto: CreateUserDto){
+        return this.userService.createUser(createUserDto);
+    }
+
+    @Patch(':id')
+    async updateUserById(
+        @Param('id', ParseIntPipe) id: number, 
+        @Body() updateUserDto: UpdateUserDto,
+    ) {
+        await this.userService.updateUser(id, updateUserDto);
+    }
+
+    @Delete(':id')
+    async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+        await this.userService.deleteUser(id);
+    }
+
+
+    @Post(':id/profiles')
+    createUserProfile(
+        @Param('id', ParseIntPipe) id: number, 
+        @Body() createUserProfileDto: CreateUserProfileDto
+    ) {
+        return this.userService.createUserProfile(id, createUserProfileDto);
+    }
+
+    @Post(':id/posts')
+    createUserPost(
+        @Param('id', ParseIntPipe) id: number, 
+        @Body() createUserPostDto: CreateUserPostDto
+    ) {
+        return this.userService.createUserPost(id, createUserPostDto);
+    }
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,22 @@
+export type CreateUserParams = {
+    username: string;
+    password: string;
+};
+
+export type UpdateUserParams = {
+    username: string;
+    password: string;
+};
+
+
+export type CreateUserProfileParams = {
+    firstName: string;
+    lastName: string;
+    age: number;
+    dob: string;
+}
+
+export class CreateUserPostParams {
+    title: string;
+    description: string;
+}
